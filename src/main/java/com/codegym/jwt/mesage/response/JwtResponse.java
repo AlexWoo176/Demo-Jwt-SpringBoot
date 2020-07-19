52d1084efbe868aeaaf49b2e254448ac7ec38acb
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-19 08:31:55 +0700

    Add Message Jwt

---
+++ src/main/java/com/codegym/jwt/mesage/response/JwtResponse.java
@@ -0,0 +1,26 @@
+package com.codegym.jwt.mesage.response;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+
+    public JwtResponse(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String tokenType) {
+        this.type = tokenType;
+    }
+}
